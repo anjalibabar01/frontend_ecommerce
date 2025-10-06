30fb64b247b75cbc59db5018b938f5cff81c120e
anjalibabar01 <anjalibabar9192@gmail.com>
2025-10-06 13:39:03 +0530

    Frontend completed with razorpay

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AddressCard = ({ address }) => {
+  if (!address) return null;
+
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">
+          {address.firstName} {address.lastName}
+        </p>
+        <p>
+          {/* This commas you can see in frontend too */}
+          {address.streetAddress}, {address.city}, {address.state},{" "}
+          {address.zipCode}
+        </p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>{address.mobile}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressCard;
